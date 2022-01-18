ad1e87ce9a821ff0069a27de2839109ea9533400
Amine.Mraihi <mraihiamin@gmail.com>
2022-01-18 09:31:47 +0100

    initial social authentication backend project

---
+++ src/main/java/com/authentication/authenticationSocialLogin/AuthenticationSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.authenticationSocialLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSocialLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentication/authenticationSocialLogin/AuthenticationSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.authenticationSocialLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
